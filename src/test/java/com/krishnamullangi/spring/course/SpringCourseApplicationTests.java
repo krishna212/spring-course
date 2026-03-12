bdf89d111726079923c92a83650c6113f0a7efd3
krishna212 <krishnasai2121999@gmail.com>
2026-03-11 23:59:04 -0700

    Initial commit: Spring course project

---
+++ src/test/java/com/krishnamullangi/spring/course/SpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishnamullangi.spring.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
